40d276059a69e8fd76d68d9312750c5dc8fe04eb
Mrudubhashini <mridubhashini.u.s@gmail.com>
2026-03-03 14:32:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode(){
+    document.body.classList.toggle("light");
+}
+
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", ()=>{
+    sections.forEach(section=>{
+        const top = section.getBoundingClientRect().top;
+        const trigger = window.innerHeight - 100;
+        if(top < trigger){
+            section.classList.add("show");
+        }
+    });
+});
